193ca3172ec9366aad6c0064d5ac7f307274ebf5
Adrian <31854027+pleaseHelpImDumb@users.noreply.github.com>
2025-05-06 12:14:29 -0400

    test commit (cursor)

--- noteworthy/src/main/java/com/noteworthy/model/Cursor.java
+++ noteworthy/src/main/java/com/noteworthy/model/Cursor.java
@@ -2,10 +2,17 @@ package com.noteworthy.model;
 
 public class Cursor {
  
-    
+    private Document doc;
+
+    private int pos;
+    private int startSelection;
+    private int endSelection;
 
 
     public Cursor(Document doc){
-        
+        this.doc = doc;
+        this.pos = 0;
+        this.startSelection = 0;
+        this.endSelection = 0;
     }
 }
